6da1f3ab5eeff1b24785885a0483a082cc6a3745
Mohab <mohabezzat1@gmail.com>
2024-04-08 07:10:54 -0500

    Feedback & Rating screen

--- lib/ui/screens/driver/driver_activity_screen.dart
+++ lib/ui/screens/driver/driver_activity_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:zipapp/constants/zip_colors.dart';
 import 'package:zipapp/constants/zip_design.dart';
-import 'package:zipapp/ui/widgets/ride_activity_item.dart';
+import 'package:zipapp/ui/screens/driver/driver_activity_item.dart';
+
+
 
 class DriverActivityScreen extends StatefulWidget {
   const DriverActivityScreen({super.key});
@@ -12,6 +14,7 @@ class DriverActivityScreen extends StatefulWidget {
 
 class _DriverActivityScreenState extends State<DriverActivityScreen> {
   List<Ride> rides = [];
+  double averageRating = 0;
 
   @override
   void initState() {
@@ -21,58 +24,74 @@ class _DriverActivityScreenState extends State<DriverActivityScreen> {
 
   void _populateRideActivityData() {
     List<Ride> tempRides = [];
+    //calculate the average rating
+
+
     tempRides = [
       Ride(
-        destination: 'Jordan Hare Stadium',
-        dateTime: DateTime(2024, 10, 12, 14, 30),
-        price: 12.23,
+          destination: 'Jordan Hare Stadium',
+          dateTime: DateTime(2024, 10, 12, 14, 30),
+          price: 12.23,
+          rating: 5.0
       ),
       Ride(
-        destination: 'Jordan Hare Stadium',
-        dateTime: DateTime(2024, 10, 12, 14, 30),
-        price: 12.23,
+          destination: 'Jordan Hare Stadium',
+          dateTime: DateTime(2024, 10, 12, 14, 30),
+          price: 12.23,
+          rating: 5.0
       ),
       Ride(
-        destination: 'Jordan Hare Stadium',
-        dateTime: DateTime(2024, 10, 12, 14, 30),
-        price: 12.23,
+          destination: 'Jordan Hare Stadium',
+          dateTime: DateTime(2024, 10, 12, 14, 30),
+          price: 12.23,
+          rating: 5.0
       ),
       Ride(
-        destination: 'Jordan Hare Stadium',
-        dateTime: DateTime(2024, 10, 12, 14, 30),
-        price: 12.23,
+          destination: 'Jordan Hare Stadium',
+          dateTime: DateTime(2024, 10, 12, 14, 30),
+          price: 12.23,
+          rating: 5.0
       ),
       Ride(
-        destination: 'Jordan Hare Stadium',
-        dateTime: DateTime(2024, 10, 12, 14, 30),
-        price: 12.23,
+          destination: 'Jordan Hare Stadium',
+          dateTime: DateTime(2024, 10, 12, 14, 30),
+          price: 12.23,
+          rating: 5.0
       ),
       Ride(
-        destination: 'Jordan Hare Stadium',
-        dateTime: DateTime(2024, 10, 12, 14, 30),
-        price: 12.23,
+          destination: 'Jordan Hare Stadium',
+          dateTime: DateTime(2024, 10, 12, 14, 30),
+          price: 12.23,
+          rating: 5.0
       ),
       Ride(
-        destination: 'Jordan Hare Stadium',
-        dateTime: DateTime(2024, 10, 12, 14, 30),
-        price: 12.23,
+          destination: 'Jordan Hare Stadium',
+          dateTime: DateTime(2024, 10, 12, 14, 30),
+          price: 12.23,
+          rating: 5.0
       ),
       Ride(
-        destination: 'Jordan Hare Stadium',
-        dateTime: DateTime(2024, 10, 12, 14, 30),
-        price: 12.23,
+          destination: 'Jordan Hare Stadium',
+          dateTime: DateTime(2024, 10, 12, 14, 30),
+          price: 12.23,
+          rating: 5.0
       ),
       Ride(
-        destination: 'Jordan Hare Stadium',
-        dateTime: DateTime(2024, 10, 12, 14, 30),
-        price: 12.23,
+          destination: 'Jordan Hare Stadium',
+          dateTime: DateTime(2024, 10, 12, 14, 30),
+          price: 12.23,
+          rating: 5.0
       ),
       Ride(
-        destination: 'Jordan Hare Stadium',
-        dateTime: DateTime(2024, 10, 12, 14, 30),
-        price: 12.23,
+          destination: 'Jordan Hare Stadium',
+          dateTime: DateTime(2024, 10, 12, 14, 30),
+          price: 12.23,
+          rating: 5.0
       ),
     ];
+    double sumRatings = tempRides.fold(0.0, (sum, item) => sum + item.rating);
+    averageRating = sumRatings / tempRides.length;
+
     setState(() {
       rides = tempRides;
     });
@@ -81,9 +100,9 @@ class _DriverActivityScreenState extends State<DriverActivityScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ZipColors.primaryBackground,
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: ZipColors.primaryBackground,
+        backgroundColor: Colors.white,
         title: const Text(
           'Activity',
           style: ZipDesign.pageTitleText,
@@ -95,25 +114,62 @@ class _DriverActivityScreenState extends State<DriverActivityScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            const Text(
-              'Past',
-              textAlign: TextAlign.left,
+            Text(
+              '\nYour Average Rating',
               style: ZipDesign.sectionTitleText,
             ),
-            rides.isNotEmpty
-                ? Expanded(
-                    child: ListView.builder(
-                      itemCount: rides.length,
-                      itemBuilder: (context, index) {
-                        return RideActivityItem(
-                          destination: rides[index].destination,
-                          dateTime: rides[index].dateTime,
-                          price: rides[index].price,
-                        );
-                      },
-                    ),
-                  )
-                : const SizedBox(),
+            SizedBox(height: 8),
+            Row(
+              children: [
+                Icon(Icons.star, color: Colors.amber, size: 40),
+                SizedBox(width: 10),
+                RichText(
+                  text: TextSpan(
+                    style: DefaultTextStyle.of(context).style,
+                    children: [
+                      TextSpan(
+                        text: '${averageRating.toStringAsFixed(1)}', // Average rating value
+                        style: TextStyle(color: Colors.black,
+                            fontSize: 28),
+                      ),
+                      TextSpan(
+                        text: ' / 5.0',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 24),
+            Divider(
+              color: Colors.grey,
+              thickness: 0.5,
+            ),
+
+            SizedBox(height: 16),
+            const Text(
+              'Completed Trips',
+              style: ZipDesign.sectionTitleText,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: rides.length,
+                itemBuilder: (context, index) {
+                  return DriverActivityItem(
+                    destination: rides[index].destination,
+                    dateTime: rides[index].dateTime,
+                    price: rides[index].price,
+
+                    // rating: rides[index].rating,
+                  );
+                },
+              ),
+            )
+
           ],
         ),
       ),
@@ -121,11 +177,23 @@ class _DriverActivityScreenState extends State<DriverActivityScreen> {
   }
 }
 
+
 class Ride {
   final String destination;
   final DateTime dateTime;
   final double price;
+  final double rating;
 
-  Ride(
-      {required this.destination, required this.dateTime, required this.price});
+
+  Ride({
+    required this.destination,
+    required this.dateTime,
+    required this.price,
+    this.rating = 5.0,
+  });
 }
+
+
+// Ensure that your RideActivityItem widget can accept and properly display
+// the destination, dateTime, and price properties.
+
---
+++ lib/ui/screens/driver/driver_details_screen.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
+import 'package:zipapp/business/user.dart';
+import 'package:zipapp/constants/tailwind_colors.dart';
+import 'package:zipapp/constants/zip_colors.dart';
+import 'package:zipapp/constants/zip_design.dart';
+import 'package:zipapp/constants/zip_formats.dart';
+
+class DriverDetailsScreen extends StatefulWidget {
+  final DateTime dateTime;
+  final double price;
+  const DriverDetailsScreen(
+      {super.key, required this.dateTime, required this.price});
+
+  @override
+  State<DriverDetailsScreen> createState() => _DriverDetailsScreenState();
+}
+
+class _DriverDetailsScreenState extends State<DriverDetailsScreen> {
+  final UserService userService = UserService();
+  // Sample static comments
+  List<String> comments = [
+    "The driver was really friendly and the car was clean.",
+    "Excellent service, would ride again!",
+    "Driver arrived late but apologized and explained the delay.",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ride Details', style: ZipDesign.pageTitleText),
+        backgroundColor: Colors.white,
+        titleSpacing: 0,
+        scrolledUnderElevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.close),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildNameDatePriceRow(),
+              const Padding(
+                padding: EdgeInsets.only(top: 32, bottom: 16),
+                child: Text('Location', textAlign: TextAlign.left, style: ZipDesign.sectionTitleText),
+              ),
+              _buildStartLocation('250 W. Glenn Ave, Auburn, AL 36830', DateTime(2024, 10, 12, 14, 35)),
+              const SizedBox(height: 16),
+              _buildEndLocation('251 S. Donahue Dr, Auburn, AL 36849', DateTime(2024, 10, 12, 14, 49)),
+              const Padding(
+                padding: EdgeInsets.only(top: 32, bottom: 16),
+                child: Text('Tip and Rating', textAlign: TextAlign.left, style: ZipDesign.sectionTitleText),
+              ),
+              _buildTipOrRatingRow(LucideIcons.coins, '\$3.40'),
+              const SizedBox(height: 16),
+              _buildTipOrRatingRow(LucideIcons.star, '4 stars'),
+              const Padding(
+                padding: EdgeInsets.only(top: 32, bottom: 16),
+                child: Text('Comments/Concerns', style: ZipDesign.sectionTitleText),
+              ),
+              Card(
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: comments.map((comment) => Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: Text(comment,style: TextStyle(color: Colors.black)),
+                    )).toList(),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom:16),
+                child: Text('Trip ID: a2sg6h-24c7hjfd-565fgng-8jge34323', style: ZipDesign.tinyLightText),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  Widget _buildNameDatePriceRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Ride with ${userService.user.firstName} ${userService.user
+                  .lastName[0]}.',
+              textAlign: TextAlign.left,
+              style: ZipDesign.sectionTitleText,
+            ),
+            ZipFormats.activityDetailsDatePriceFormatter(
+              widget.dateTime, widget.price,),
+          ],
+        ),
+        Container(
+          height: 56,
+          width: 56,
+          decoration: BoxDecoration(
+            color: TailwindColors.gray300,
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: const Icon(LucideIcons.user,
+              color: TailwindColors.gray500, size: 24),
+        )
+      ],
+    );
+  }
+
+  Widget _buildStartLocation(String address, DateTime dateTime) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(LucideIcons.locate, size: 16, color: Colors.black),
+            const SizedBox(width: 16),
+            Text(address,
+                style:
+                const TextStyle(fontSize: 14, fontWeight: FontWeight.w400)),
+          ],
+        ),
+        ZipFormats.activityDetailsTimeFormatter(dateTime),
+      ],
+    );
+  }
+
+  Widget _buildEndLocation(String address, DateTime dateTime) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(LucideIcons.mapPin, size: 16, color: Colors.black),
+            const SizedBox(width: 16),
+            Text(address,
+                style:
+                const TextStyle(fontSize: 14, fontWeight: FontWeight.w400)),
+          ],
+        ),
+        ZipFormats.activityDetailsTimeFormatter(dateTime),
+      ],
+    );
+  }
+
+  Widget _buildTipOrRatingRow(IconData icon, String title) {
+    return Row(
+      children: [
+        Row(
+          children: <Widget>[
+            Icon(icon, size: 16, color: Colors.black),
+            const SizedBox(width: 16),
+            Text(
+              title,
+              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+}
